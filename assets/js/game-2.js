9ca8014fb1b81443e4954a06bc46aadc05474b5f
Rhonalf Martinez <rhonaldomaster@gmail.com>
2020-04-14 09:01:38 -0500

    add movements and enemies

--- assets/js/game-2.js
+++ assets/js/game-2.js
@@ -1,5 +1,6 @@
 var game = null;
 var player = null;
+var aliens = null;
 var cursors = null;
 var platforms = null;
 
@@ -36,6 +37,19 @@ function iniciarJugador(game) {
   });
 
   game.anims.create({
+    key: 'up',
+    frames: [{ key: 'granjero', frame: 6 }],
+    frameRate: 10,
+    repeat: -1
+  });
+
+  game.anims.create({
+    key: 'down',
+    frames: [{ key: 'granjero', frame: 6 }],
+    frameRate: 10
+  });
+
+  game.anims.create({
     key: 'turn',
     frames: [{ key: 'granjero', frame: 6 }],
     frameRate: 20
@@ -49,21 +63,45 @@ function iniciarJugador(game) {
   });
 }
 
+function iniciarAliens(game) {
+  var xPosition = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+  aliens = game.physics.add.group({
+    key: 'alien',
+    repeat: 11,
+    setXY: { x: xPosition, y: 400, stepX: 70 }
+  });
+  aliens.children.iterate(function (alien) {
+    alien.setBounce(0.2);
+    alien.setCollideWorldBounds(true);
+    alien.setVelocity(Phaser.Math.Between(-200, 200), 20);
+  });
+}
+
 function preload() {
+  this.load.spritesheet('alien', 'assets/img/alien.png', { frameWidth: 68, frameHeight: 180 });
   this.load.image('fondo', 'assets/img/fondobig.jpg');
   this.load.spritesheet('granjero', 'assets/img/personaje_run.png', { frameWidth: 150, frameHeight: 150 });
-  this.load.image('ground', 'assets/img/platform.png');
 }
 
 function create() {
   this.add.image(590, 363, 'fondo');
   iniciarJugador(this);
+  iniciarAliens(this);
   cursors = this.input.keyboard.createCursorKeys();
   platforms = this.physics.add.staticGroup();
+  this.physics.add.collider(player, aliens, hitAlien, null, this);
+}
+
+function hitAlien(player, alien) {
+  this.physics.pause();
+  player.setTint(0xff0000);
+  player.anims.play('turn');
+  gameOver = true;
 }
 
 function update() {
   let velocityX = 0;
+  let velocityY = 0;
   if (cursors.left.isDown) {
     velocityX = -160;
     player.anims.play('left', true);
@@ -73,11 +111,16 @@ function update() {
   } else {
     player.anims.play('turn');
   }
-  player.setVelocityX(velocityX);
 
-  if (cursors.up.isDown && player.body.touching.down) {
-    player.setVelocityY(-330);
+  if (cursors.up.isDown) {
+    velocityY = -160;
+    player.anims.play('up', true);
+  } else if (cursors.down.isDown) {
+    velocityY = 160;
+    player.anims.play('down', true);
   }
+  player.setVelocityX(velocityX);
+  player.setVelocityY(velocityY);
 }
 
 function init() {
